0fc51c678d40619fbb85983fdb5b87dac35c1938
Christof Marti <chrmarti@microsoft.com>
2022-09-02 14:41:03 +0000

    test image

---
+++ docker-only/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-context context2=./context2 -f Dockerfile context1
+
+docker push ghcr.io/chrmarti/cache-from-test/images/test-cache:latest
