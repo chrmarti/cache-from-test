92d536d34da8247e04f25f33ea3d58d2f4817100
Christof Marti <chrmarti@microsoft.com>
2022-09-02 21:05:55 +0000

    test with updated permissions

--- docker-only/build.sh
+++ docker-only/build.sh
@@ -8,4 +8,4 @@ chmod -R g+w .
 
 ls -la *
 
-docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest -f Dockerfile context1
+docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg _DEV_CONTAINERS_BASE_IMAGE=dev_container_auto_added_stage_label --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-context dev_containers_feature_content_source=./context2 -f Dockerfile context1
