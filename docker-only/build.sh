5cb8786033ba8715fc2ce5d4d029ecf0ed2420f1
Christof Marti <chrmarti@microsoft.com>
2022-09-02 20:34:02 +0000

    show permissions

--- docker-only/build.sh
+++ docker-only/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+ls -la *
+
 docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest -f Dockerfile context1
