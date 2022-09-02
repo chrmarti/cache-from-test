6a55c3156b1f308c30f28c58e064d8688d7bdb60
Christof Marti <chrmarti@microsoft.com>
2022-09-02 21:00:30 +0000

    chmod

--- docker-only/build.sh
+++ docker-only/build.sh
@@ -4,6 +4,8 @@ umask
 
 groups
 
+chmod -R g+w .
+
 ls -la *
 
 docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest -f Dockerfile context1
