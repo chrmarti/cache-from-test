0bfb59d7ed6139b0ca3ab88e7aa53d81c0f1126f
Christof Marti <chrmarti@microsoft.com>
2022-09-02 20:00:48 +0000

    update

--- docker-only/build.sh
+++ docker-only/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg _DEV_CONTAINERS_BASE_IMAGE=dev_container_auto_added_stage_label --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-context dev_containers_feature_content_source=./context2 -f Dockerfile context1
+docker buildx build --load -t ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg _DEV_CONTAINERS_BASE_IMAGE=dev_container_auto_added_stage_label --cache-from ghcr.io/chrmarti/cache-from-test/images/test-cache:latest --build-context context2=./context2 -f Dockerfile context1
